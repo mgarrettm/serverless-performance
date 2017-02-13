30e3b3ef976a314667d6582b5f5206dcad2c6c85
Garrett McGrath <Garrett McGrath>
2017-02-13 16:14:17 -0500

    Improve support of custom GCF plugin

--- scripts/setup.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-
-fs.createReadStream('scripts/res/createFunctions.js').pipe(fs.createWriteStream('node_modules/serverless-google-cloudfunctions/deploy/lib/createFunctions.js'));
