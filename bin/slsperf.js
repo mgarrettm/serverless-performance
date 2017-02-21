4a8d06b40e60bf5472fff176301462ec19ee4c52
mgarrettm <garrett.mcgrath@gmail.com>
2017-02-21 12:16:57 -0800

    fix config file write

--- bin/slsperf.js
+++ bin/slsperf.js
@@ -66,7 +66,7 @@ if (!fs.existsSync(config.resultsDir)) {
   fs.mkdirSync(config.resultsDir);
 }
 
-fs.writeFileSync(path.join(config.resultsDir, `${config.provider.name}_${config.test.type}_config.json`))
+fs.writeFileSync(path.join(config.resultsDir, `${config.provider.name}_${config.test.type}_config.json`), config);
 
 let iteration = 0;
 fs.readdirSync(config.resultsDir).forEach(file => {
